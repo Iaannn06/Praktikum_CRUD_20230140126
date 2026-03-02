13fb5787189d1b0c5deafff6a1ae0dc7a42b70fa
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-03-02 15:08:21 +0700

    revisi application

--- src/main/java/com/example/praktikum1/Praktikum1Application.java
+++ src/main/java/com/example/praktikum1/Praktikum1Application.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Praktikum1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Praktikum1Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Praktikum1Application.class, args); }
 }
