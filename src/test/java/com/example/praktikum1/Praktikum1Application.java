536ee21302388fa9a3152ccd7c06e0e0a5e53156
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-03-02 15:05:16 +0700

    revisi Praktikum1Application.java

--- src/test/java/com/example/praktikum1/Praktikum1Application.java
+++ src/test/java/com/example/praktikum1/Praktikum1Application.java
@@ -3,8 +3,9 @@ package com.example.praktikum1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.praktikum1", "com.deploy.praktikum1"})
 public class Praktikum1Application {
     public static void main(String[] args) {
-        SpringApplication.run(Praktikum1Application.class, args); }
+        SpringApplication.run(Praktikum1Application.class, args);
+    }
 }
